59a6861fa2ab952b4a244d44c1663287c1c31669
PierdomenicoPacilio <piero.pacilio@gmail.com>
2025-03-06 10:42:52 +0100

    feat: page layout

--- js/app.js
+++ js/app.js
@@ -32,16 +32,42 @@ document.getElementById('btn').addEventListener('click', function(event){
     // Output:
     let output = document.createDocumentFragment();
     if (isNaN(userKm) || isNaN(userAge) || userKm <= 0 || userAge <= 0){
-        output = (`informazioni fornite non valide.
-            possibili motivazioni:
-            - anni utente < 0
-            - anni utente != number
-            - numero chilometri < 0
-            - numero di chilometri != number`);
+        output = (`<div class="container d-flex justify-content-center mx-auto col-8 p-3 bg-light border border-5">
+                <h3>Ops, c'è stato un problema</h3>
+            </div>`);
     } else{
-        output = `<div class="container row g-2 justify-content-around mx-auto col-8 p-3 bg-light border border-5">
-        Il prezzo finale per l'utente di età ${userAge} e distanza da percorrere di ${userKm}Km è di €${finalPrice.toFixed(2)}
-        </div>`;
+        output = `<div class="container row justify-content-around mx-auto col-8 p-3 bg-light border border-5">
+                <div class="col-3 border-top border-bottom border-black border-3">
+                     <img src="img/untitled_720.png" alt="qr code"style="height: 150px;">
+                </div>
+                <div class="col-9 border-top border-bottom border-black border-3 row">
+                    <div class="row">
+                        <div class="col-4">
+                            <h5>Passeggero:</h5>
+                            <p>${userName}</p>
+                        </div>
+                        <div class="col-4">
+                            <h5>Costo:</h5>
+                            <p>€${finalPrice.toFixed(2)}</p>
+                        </div>
+                        <div class="col-4">
+                            <h5>Specifiche:</h5>
+                            <p>Numero Treno: ${Math.floor(Math.random() * (99999 - 12345 + 1)) + 12345}</p>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-4">
+                            <p>Anni: ${userAge}</p>
+                        </div>
+                        <div class="col-4">
+                            <p>Km: ${userKm}</p>
+                        </div>
+                        <div class="col-4">
+                            <p>Carrozza: ${Math.floor(Math.random() * 10) + 1}</p>
+                        </div>                   
+                    </div>  
+                </div>
+             </div>`;
     };
     document.getElementById('output').innerHTML = output;
 });
